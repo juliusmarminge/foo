dc4dfdd9e35724a57ea55913071151f91040ce67
Gerard Siles <gerardsiles@gmail.com>
2023-11-16 15:28:27 +0000

    error on utils/uploadthing.ts

---
+++ src/utils/uploadthing.ts
@@ -0,0 +1,5 @@
+import { generateComponents } from "@uploadthing/react";
+import type { OurFileRouter } from "~/server/uploadthing";
+
+export const { UploadButton, UploadDropzone, Uploader } =
+  generateComponents<OurFileRouter>();
